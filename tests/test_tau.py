4b44c8a9d6bee56d04d0bf0bfee99292052b3a29
Madsen95 <bmadse18@student.aau.dk>
2022-04-20 15:21:39 +0200

    Test setup

--- tests/test_tau.py
+++ tests/test_tau.py
@@ -1,6 +1,27 @@
 import numpy as np
 import pytest
 from src.radar import Radar
-from src.transmitter import Transmitter
 from src.receiver import Receiver
+from src.transmitter import Transmitter
+
+@pytest.fixture
+def radar():
+    tx = Transmitter()
+    rx = Receiver()
+    radar = Radar(tx, rx, "tdm", 2000)
+    return radar
+
+def test_output_scalar_dtype(radar):
+    theta = np.array(np.array([[1], [1], [1], [1]]))   
+    tau = radar.time_delay(theta, 1, 0)
+    
+    assert type(tau) == np.ndarray
+    assert type(tau[0]) == np.float64
+
+def test_output_vector_dtype(radar):
+    theta = np.array(np.array([[1], [1], [1], [1]]))   
+    tau = radar.time_delay(theta, 1, 0)
+    
+    assert type(tau) == np.ndarray
+    assert type(tau[0]) == np.float64
 
